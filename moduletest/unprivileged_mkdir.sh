7485934827f4d5d1dc43d572508d6a9b3b78ba06
anordal <andreas_nordal_4@hotmail.com>
2016-10-04 20:53:19 +0200

    build: Reasonably test writability without writing

--- moduletest/unprivileged_mkdir.sh
+++ moduletest/unprivileged_mkdir.sh
@@ -2,3 +2,4 @@
 
 # Shall fail
 ! XDG_RUNTIME_DIR=/root "$1" run true 2> /dev/null
+! XDG_RUNTIME_DIR=/root "$1" build true 2> /dev/null
--- moduletest/writability.sh
+++ moduletest/writability.sh
@@ -20,3 +20,10 @@ t=( sh -ec 'echo fail > /dev/null && cat /dev/null' )
 test "$("${s[@]}" "${t[@]}")" = ''
 test "$("${s4[@]}" "${t[@]}")" = ''
 test "$("${s16[@]}" "${t[@]}")" = ''
+
+mount_rw_check=( sh -c 'while read dev mnt type opt dump pass; do test "$mnt" = / || continue; for i in ${opt//,/ }; do test "$i" = rw && exit 0; done; done < /proc/mounts; false' )
+
+"$1" build "${mount_rw_check[@]}"
+! "$1" run "${mount_rw_check[@]}"
+! "$1" build "$1" run "${mount_rw_check[@]}"
+! "$1" run "$1" build "${mount_rw_check[@]}"
